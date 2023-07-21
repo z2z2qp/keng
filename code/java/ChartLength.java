b2bf5de005d5a4a2e388f7f80ebd790ba36acaeb
庄佳彬 <zhuangjiabin@jrizju.com>
2023-07-21 10:11:05 +0800

    长度不为1的字符

---
+++ code/java/ChartLength.java
@@ -0,0 +1,12 @@
+public class ChartLength {
+
+    public static void main(String[] agrs) {
+        System.out.println("𝄞".length());// 2
+        System.out.println("🔙".length());// 2
+        System.out.println("🚸".length());// 2
+        System.out.println("🛄".length());// 2
+        System.out.println("🦼".length());// 2
+        System.out.println("🩹".length());// 2
+        System.out.println("🛥️".length());// 3
+    }
+}
